19257c243654ca0d4605404ca4c39b568cc588dd
Matt Juskelis <matthewjuskelis@gmail.com>
2016-06-06 00:27:37 -0700

    Initial and LOD Sphere

---
+++ Planet Generation/Assets/My Assets/Scripts/Tester.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter))]
+public class Tester : MonoBehaviour
+{
+    [Range(0,6)]
+    [SerializeField] private int subdivisions = 0;
+    [SerializeField] private float radius = 1f;
+
+    private void Awake()
+    {
+        GetComponent<MeshFilter>().mesh = OctahedronSphereCreator.Create(subdivisions, radius);
+    }
+
+    private void onValidate()
+    {
+        if (subdivisions < 0) subdivisions = 0;
+        if (radius < 1f) radius = 1f;
+    }
+}
