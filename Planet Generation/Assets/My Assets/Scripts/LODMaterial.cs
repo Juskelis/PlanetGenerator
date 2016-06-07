b91dc75a4faa95ba075862ff61c8a836550b992f
Matt Juskelis <matthewjuskelis@gmail.com>
2016-06-07 16:55:09 -0700

    LOD Material Editing added

---
+++ Planet Generation/Assets/My Assets/Scripts/LODMaterial.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LODMaterial : MonoBehaviour
+{
+
+    [SerializeField] private Material mat;
+    private Material previousMat = null;
+
+	// Use this for initialization
+	void Update () {
+	    if (previousMat != mat)
+	    {
+	        previousMat = mat;
+	        foreach (var renderer in GetComponentsInChildren<MeshRenderer>())
+	        {
+	            renderer.material = mat;
+	        }
+	    }
+	}
+}
